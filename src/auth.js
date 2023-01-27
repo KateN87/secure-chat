cc9eef4db61224cb61d6c7728621a422025c5250
Kate Nordqvist <kate.nordqvist@gmail.com>
2023-01-27 12:32:58 +0100

    sign up frontend done

--- src/auth.js
+++ src/auth.js
@@ -1,32 +1,25 @@
 import jwt from 'jsonwebtoken';
 import { db } from './database.js';
-import bcrypt from 'bcryptjs'
+import bcrypt from 'bcryptjs';
 
 function authenticateUser(userName, password) {
-    console.log('BACK authenticateUser() userData', db.data.userData);
-    let match = db.data.userData.find(
-        (user) => user.userName === userName)
-    
-    if(!match){
+    /* console.log('BACK authenticateUser() userData', db.data.userData); */
+    let match = db.data.userData.find((user) => user.userName === userName);
+
+    if (!match) {
         return false;
     } else {
-        let correctPassword = bcrypt.compareSync(password, match.password)
-			if( correctPassword ) {
-				return true;
-			} 
+        let correctPassword = bcrypt.compareSync(password, match.password);
+        console.log('BACK AuthenticateUser match.password', match.password);
+        if (correctPassword) {
+            console.log(
+                'BACK AuthenticateUser correctPassword',
+                correctPassword
+            );
+            return true;
+        }
     }
 }
-
-/* let match = users.find(user => user.username == username)
-		if( !match ) {
-			console.log('> Wrong username\n')
-		} else {
-			let correctPassword = bcrypt.compareSync(password, match.password)
-			if( correctPassword ) {
-				console.log('> Welcome user!')
-			} else {
-				console.log('> Password does not match.')
-			} */
 
 function createToken(userName) {
     const user = { userName: userName };
@@ -36,5 +29,4 @@ function createToken(userName) {
     return user;
 }
 
-
-export { createToken, authenticateUser};
+export { createToken, authenticateUser };

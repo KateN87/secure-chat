b0082bdf410e42ac0be31503c11f40a3980c4a13
Kate Nordqvist <kate.nordqvist@gmail.com>
2023-01-28 18:32:17 +0100

    post newChannel & put message backend done

---
+++ static/editRemove.js
@@ -0,0 +1,37 @@
+import { checkAuth } from './auth.js';
+import { getMessages } from './script.js';
+
+async function removeMessage(name, loggedInUser, element) {
+    let deleteItem = {
+        name: name.name,
+        user: loggedInUser.userName,
+    };
+    if (checkAuth) {
+        try {
+            const response = await fetch('/api/private/' + element.id, {
+                method: 'DELETE',
+                body: JSON.stringify(deleteItem),
+                headers: {
+                    'Content-type': 'application/json',
+                },
+            });
+
+            if (response.status === 200) {
+                getMessages(name);
+            } else {
+                console.log('Could not remove. Status: ', response.status);
+            }
+        } catch (error) {
+            console.log(
+                'Could not DELETE data from the server. Error message: ' +
+                    error.message
+            );
+        }
+    } else {
+        console.log('You are not logged in');
+    }
+}
+
+async function editMessage(name, loggedInUser, element) {}
+
+export { removeMessage, editMessage };
